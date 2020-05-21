e6eebd20b90dd3a18de1145ed287bf99ad951348
EduGomes18 <eduardo_gomes1318@outlook.com>
2020-05-21 19:42:43 -0300

    Modify game ideia to zombies

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-let canvas = document.getElementById("snake");
+let canvas = document.getElementById("zombie");
 let context = canvas.getContext("2d");
 let box = 32;
+
+let zombie = [];
+
+zombie[0] = {
+  x: 8 * box,
+};
 
 function createBg() {
   context.fillStyle = "#f1f";
 
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
+
+function createZombie() {}
 
 createBg();
